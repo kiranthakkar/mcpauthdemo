9363b79adeab8290526a7f80d600c0be840f0aaf
kiranthakkar <thakkar.kiran@gmail.com>
2025-11-06 23:10:09 -0800

    Updated Doc and code

--- ociprovider.py
+++ ociprovider.py
@@ -5,10 +5,20 @@ The pull request for the provider is submitted to fastmcp.
 This module provides OIDC Implementation to integrate MCP servers with OCI.
 You only need OCI Identity Domain's discovery URL, client ID, client secret, and base URL.
 
+Post Authentication, you get OCI IAM domain access token. That is not authorized to invoke OCI control plane.
+You need to exchange the IAM domain access token for OCI UPST token to invoke OCI control plane APIs.
+The sample code below has get_oci_signer function that returns OCI TokenExchangeSigner object. 
+You can use the signer object to create OCI service object.
+
 Example:
     ```python
     from fastmcp import FastMCP
     from fastmcp.server.auth.providers.ociprovider import OCIProvider
+    
+    import oci
+    from oci.auth.signers import TokenExchangeSigner
+    
+    _global_token_cache = {} #In memory cache for OCI session token signer
 
     # Simple OCI OIDC protection
     auth = OCIProvider(
@@ -17,6 +27,31 @@ Example:
         client_secret="oci-iamdomain-app-client-secret",
         base_url="http://localhost:8000",
     )
+    
+    def get_oci_signer() -> TokenExchangeSigner:
+    
+        #Check if the signer exists for the token ID in memory cache
+        cached_signer = _global_token_cache.get(tokenID)
+        logger.debug(f"Global cached signer: {cached_signer}")
+        if cached_signer:
+            logger.debug(f"Using globally cached signer for token ID: {tokenID}")
+            return cached_signer
+
+        #If the signer is not yet created for the token then create new OCI signer object
+        logger.debug(f"Creating new signer for token ID: {tokenID}")
+        signer = TokenExchangeSigner(
+            jwt_or_func=token,
+            oci_domain_id=IAM_GUID,
+            client_id=IAM_TOKENEXCHANGE_CLIENT_ID,
+            client_secret=IAM_TOKENEXCHANGE_CLIENT_SECRET
+        )
+        logger.debug(f"Signer {signer} created for token ID: {tokenID}")
+        
+        #Cache the signer object in memory cache
+        _global_token_cache[tokenID] = signer
+        logger.debug(f"Signer cached for token ID: {tokenID}")
+
+        return signer
 
     mcp = FastMCP("My Protected Server", auth=auth)
     ```
@@ -116,7 +151,7 @@ class OCIProvider(OIDCProxy):
                 if v is not NotSet
             }
         )
-
+        
         if not settings.config_url:
             raise ValueError(
                 "config_url is required - set via parameter or FASTMCP_SERVER_AUTH_OCI_CONFIG_URL"
@@ -153,6 +188,8 @@ class OCIProvider(OIDCProxy):
             jwt_signing_key=settings.jwt_signing_key,
             require_authorization_consent=require_authorization_consent,
         )
+        
+        
 
         logger.debug(
             "Initialized OCI OAuth provider for client %s with scopes: %s",
--- server.py
+++ server.py
@@ -2,93 +2,108 @@ import os
 from dotenv import load_dotenv
 
 import oci
+
+from fastmcp import Context, FastMCP
+from fastmcp.server.auth.oidc_proxy import OIDCProxy
+from fastmcp.server.dependencies import get_access_token
 from oci.auth.signers import TokenExchangeSigner
 
 from starlette.responses import PlainTextResponse
 from starlette.requests import Request
-from fastmcp import FastMCP
-from fastmcp.server.dependencies import get_access_token
-from fastmcp.server.auth import OIDCProxy
-from fastmcp.server.context import Context
-from fastmcp.utilities.logging import get_logger
 
-logger = get_logger(__name__)
-
-# Store tokens on disk for persistence across restarts
-#token_storage = DiskCache(cache_dir="/Users/kgthakka/Downloads/agentdemo/sessionstore", ttl_hours=24)
-#token_storage = RedisTokenCache(redis_url="redis://{REDIS_SERVER}:6379") 
-#token_storage = InMemoryContextCache()
-_global_token_cache = {} #In memory cache for OCI session token signer
-
+# Load Environment variables from .env file
 load_dotenv()
-IAM_DOMAIN = os.getenv("IAM_DOMAIN")
-IAM_CLIENT_ID = os.getenv("IAM_CLIENT_ID")
-IAM_CLIENT_SECRET = os.getenv("IAM_CLIENT_SECRET")
+# Create .env file with IDCS_DOMAIN, IDCS_CLIENT_ID, IDCS_CLIENT_SECRET variables.
+# IDCS_CLIENT_ID and IDCS_CLIENT_SECRET are from the IAM Domain OAuth2 client credentials.
+# IDCS_DOMAIN is the domain name of the created IAM Domain.
+IDCS_DOMAIN = os.getenv("IDCS_DOMAIN")
+IDCS_CLIENT_ID = os.getenv("IDCS_CLIENT_ID")
+IDCS_CLIENT_SECRET = os.getenv("IDCS_CLIENT_SECRET")
 
-IAM_GUID = os.getenv("IAM_GUID")
-IAM_TOKENEXCHANGE_CLIENT_ID = os.getenv("IAM_TOKENEXCHANGE_CLIENT_ID")
-IAM_TOKENEXCHANGE_CLIENT_SECRET = os.getenv("IAM_TOKENEXCHANGE_CLIENT_SECRET")
+# Simple in-memory global cache for signers
+# In production, consider using a more robust caching mechanism
+_global_token_cache = {}
 
-auth = OIDCProxy(
-    config_url= f"https://{IAM_DOMAIN}/.well-known/openid-configuration",
-    client_id=IAM_CLIENT_ID,
-    client_secret=IAM_CLIENT_SECRET,
-    base_url="http://localhost:8000",
-    required_scopes=["openid", "profile", "email"],
-    #require_authorization_consent=False,
-)
-mcp = FastMCP("My MCP Server", auth=auth)
-
-def get_oci_signer(token: str, tokenID: str) -> TokenExchangeSigner:
+# Get an instance of OCI Token Exchange Signer
+def get_oci_signer() -> TokenExchangeSigner:
     """Create an OCI TokenExchangeSigner using the provided token."""
     
-    #Check if the signer exists for the token ID in memory cache
+    mcp_token = get_access_token()
+    tokenID = mcp_token.claims.get("jti")
+    token = mcp_token.token 
+    
     cached_signer = _global_token_cache.get(tokenID)
-    logger.debug(f"Global cached signer: {cached_signer}")
+    print(f"Global cached signer: {cached_signer}")
     if cached_signer:
-        logger.debug(f"Using globally cached signer for token ID: {tokenID}")
+        print(f"Using globally cached signer for token ID: {tokenID}")
         return cached_signer
-
-    #If the signer is not yet created for the token then create new OCI signer object
-    logger.debug(f"Creating new signer for token ID: {tokenID}")
+    print(f"Creating new signer for token ID: {tokenID}")
     signer = TokenExchangeSigner(
         jwt_or_func=token,
-        oci_domain_id=IAM_GUID,
-        client_id=IAM_TOKENEXCHANGE_CLIENT_ID,
-        client_secret=IAM_TOKENEXCHANGE_CLIENT_SECRET
+        oci_domain_id=IDCS_DOMAIN.split(".")[0],
+        client_id=IDCS_CLIENT_ID,
+        client_secret=IDCS_CLIENT_SECRET,
     )
-    logger.debug(f"Signer {signer} created for token ID: {tokenID}")
-    
-    #Cache the signer object in memory cache
     _global_token_cache[tokenID] = signer
-    logger.debug(f"Signer cached for token ID: {tokenID}")
-
+    print(f"Signer cached globally for token ID: {tokenID}")
     return signer
 
+auth = OIDCProxy(
+    config_url=f"https://{IDCS_DOMAIN}/.well-known/openid-configuration",
+    client_id=IDCS_CLIENT_ID,
+    client_secret=IDCS_CLIENT_SECRET,
+    # FastMCP endpoint
+    base_url="http://localhost:8000",
+    # audience=IDCS_CLIENT_ID,
+    required_scopes=["openid", "profile", "email"],
+    # redirect_path="/custom/callback",
+)
+
+mcp = FastMCP(name="My Server", auth=auth)
+
 @mcp.tool
-def get_os_namespace(region: str, ctx: Context) -> str:
-    """Get OCI Object Storage namespace for the tenancy"""
+async def list_regions(region: str, ctx: Context) -> str:
+    """List all OCI regions available for the tenancy
+    Input: region (str)
+    Output: regions (str)
+    """
     
-    """First create OCI Object storage client. 
-    To create the client, we need OCI signer. 
+    """Create OCI Object storage client using token exchange signer. 
     We will exchange IAM domain JWT token for OCI UPST token and use the UPST token to create signer object.
     """
-    token = get_access_token()
-    tokenID = token.claims.get("jti")
-    ac_token = token.token
-    signer = get_oci_signer(ac_token, tokenID)
+    signer = get_oci_signer()
+    iam_client = oci.identity.IdentityClient(config={'region': region}, signer=signer)
+
+    # Get the regions from the identity client
+    regions = iam_client.list_regions().data
+    print(f"Regions are {regions}")
+    return regions.__str__()
+
+@mcp.tool
+async def get_os_namespace(region: str, ctx: Context) -> str:
+    """Get OCI Object Storage namespace for the tenancy
+    Input: region (str)
+    Output: namespace (str)
+    """
+    
+    """Create OCI Object storage client using token exchange signer. 
+    We will exchange IAM domain JWT token for OCI UPST token and use the UPST token to create signer object.
+    """
+    
+    signer = get_oci_signer()
     object_storage_client = oci.object_storage.ObjectStorageClient(config={'region': region}, signer=signer)
 
-    # Get the namespace using Object Storage Client
+    # Get the namespace
     namespace_response = object_storage_client.get_namespace()
     namespace_name = namespace_response.data
     return namespace_name
 
 @mcp.tool
 def whoami(ctx: Context) -> str:
-    """The whoami function is to test MCP server without requiring token exchange.
+    """The whoami tool is to test MCP server without requiring token exchange.
     This tool can be used to test successful authentication against OCI IAM.
     It will return logged in user's subject (username from IAM domain)."""
+    
     token = get_access_token()
     user = token.claims.get("sub")
     return f"You are User: {user}"
@@ -96,12 +111,14 @@ def whoami(ctx: Context) -> str:
 @mcp.tool
 async def get_token() -> str:
     """This tool can be used to get logged in user's IAM domain access token."""
+    
     token = get_access_token()
     return token.token
 
 @mcp.tool
 async def get_access_token_claims() -> dict:
     """This tool can be used to get the authenticated user's access token claims."""
+    
     token = get_access_token()
     return {
         "sub": token.claims.get("sub"),
@@ -113,9 +130,7 @@ async def get_access_token_claims() -> dict:
 
 if __name__ == "__main__":
     mcp.run(transport="http", port=8000)
-    
+
 @mcp.custom_route("/health", methods=["GET"])
 async def health_check(request: Request) -> PlainTextResponse:
-    """This custom route can be used to run health check against MCP server.
-    If you deploy load balancer, it can use /health URL to run the health check."""
     return PlainTextResponse("OK")
